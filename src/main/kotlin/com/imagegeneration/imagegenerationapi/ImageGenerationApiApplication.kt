09ea9d3394e39fe5fd036d2981815aa80569516e
xymon2 <vbbn2030@gmail.com>
2023-04-11 18:35:04 +0900

    initial commit with spring setting

---
+++ src/main/kotlin/com/imagegeneration/imagegenerationapi/ImageGenerationApiApplication.kt
@@ -0,0 +1,11 @@
+package com.imagegeneration.imagegenerationapi
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ImageGenerationApiApplication
+
+fun main(args: Array<String>) {
+	runApplication<ImageGenerationApiApplication>(*args)
+}
---
+++ src/test/kotlin/com/imagegeneration/imagegenerationapi/ImageGenerationApiApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.imagegeneration.imagegenerationapi
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ImageGenerationApiApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
